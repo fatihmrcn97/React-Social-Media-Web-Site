4604aabb1ab60b901ca57ea1aea2df754de52e38
fatihmrcn97 <fatihmercan7991@gmail.com>
2021-04-01 17:03:43 +0300

    nothing changed

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect, Component } from "react";
+import Post from "../../containers/post";
 import { UserContext } from "../../contexts/user";
 import { firebasedb } from "../../firebase";
 
@@ -8,6 +9,18 @@ export default function Profile() {
   const [image, setimage] = useState("");
   const [phoneNumber, setphoneNumber] = useState("");
   const [userFromFb, setuserFromFb] = useState([]);
+  const [posts, setPosts] = useState({});
+ 
+
+  useEffect(() => {
+    firebasedb.ref("Posts").on("value", (snapshot) => {
+      if (snapshot.val()) {
+        setPosts({
+          ...snapshot.val(),
+        });
+      }
+    });
+  }, []);
 
   useEffect(() => {
     firebasedb.ref("Users").on("value", (snapshot) => {
@@ -33,7 +46,8 @@ export default function Profile() {
   }, )
 
   return (
-    <div className="row py-5 px-4">
+   <div>
+      <div className="row py-5 px-4">
       <div className="col-md-5 mx-auto">
         {/* Profile widget */}
         <div className="bg-white shadow rounded overflow-hidden">
@@ -49,11 +63,11 @@ export default function Profile() {
               className="media align-items-end profile-head"
               style={{ transform: "translateY(5rem)" }}
             >
-              <div className="profile mr-3">
+              <div className="mr-3">
                 <img
                   src={image}
-                  alt="..."
-                  className="rounded mb-2 img-thumbnail imagesiz"
+                  alt="profileimage"
+                  className="rounded mb-2 img-thumbnail imagesiz profile"
                 />
                 <a href="#" className="btn btn-outline-dark btn-sm btn-block">
                   Edit profile
@@ -102,47 +116,38 @@ export default function Profile() {
               <p className="font-italic mb-0">Lives in Turkey</p>
               <p className="font-italic mb-0">Photographer</p>
             </div>
-          </div>
-          <div className="py-4 px-4">
-            <div className="d-flex align-items-center justify-content-between mb-3">
-              <h5 className="mb-0">Recent photos</h5>
-              <a href="#" className="btn btn-link text-muted">
-                Show all
-              </a>
-            </div>
-            <div className="row">
-              <div className="col-lg-6 mb-2 pr-lg-1">
-                <img
-                  src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                  alt=""
-                  className="img-fluid rounded shadow-sm"
-                />
-              </div>
-              <div className="col-lg-6 mb-2 pl-lg-1">
-                <img
-                  src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                  alt=""
-                  className="img-fluid rounded shadow-sm"
-                />
-              </div>
-              <div className="col-lg-6 pr-lg-1 mb-2">
-                <img
-                  src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                  alt=""
-                  className="img-fluid rounded shadow-sm"
-                />
-              </div>
-              <div className="col-lg-6 pl-lg-1">
-                <img
-                  src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                  alt=""
-                  className="img-fluid rounded shadow-sm"
-                />
-              </div>
-            </div>
-          </div>
+          </div>      
         </div>
       </div>
     </div>
+
+    {Object.keys(posts).map((id) => {
+        if(posts[id].uid === user.uid){
+          return (
+          <Post
+            profileUrl={posts[id].uDp}
+            username={posts[id].uName}
+            photoURL={posts[id].pImage}
+            caption={posts[id].pDescription}
+            id={id}
+            key={id}
+            comments={posts[id].Comments}
+            likes ={posts[id].pLikes}
+            commentCount ={posts[id].pComments}
+            time ={posts[id].pTime}
+            title = {posts[id].pTitle}
+            postId = {posts[id].uid}
+            pId = {posts[id].pId}
+          ></Post>
+        );
+        }
+        else{
+          return (<></>);
+        }
+      })}
+    
+    
+   </div>
+    
   );
 }
